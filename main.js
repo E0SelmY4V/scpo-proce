e253aa84836bc5f4c2ed447e8ccd472e914fb187
E0SelmY4V-PC <2507917746@qq.com>
2022-12-05 22:29:02 +0800

    add: 框架Proce

--- main.js
+++ main.js
@@ -43,6 +43,17 @@
 	}
 	pipe.arrayFrom = arrayFrom;
 
+	function getList(list) {
+		return isArrayLike(list[0]) ? list[0] : list;
+	}
+	pipe.getList = getList;
+
+	var m = false, z = 0, f = 1;
+	function getId() {
+		return (m = !m) ? z-- : f++;
+	}
+	pipe.getId = getId;
+
 	function ProceArray() {
 		this.index = {};
 	}
@@ -89,12 +100,77 @@
 	pipe.ConfigClass = ConfigClass;
 	pipe.config = new ConfigClass();
 
-	function getList(list) {
-		return isArrayLike(list[0]) ? list[0] : list;
+	function Proce(doexpr, config, cleared) {
+		this.queuetodo = [], this.queueordo = [], this.config = new ConfigClass(config, this), this.id = getId();
+		cleared ? this.cleared = true : typeof doexpr === 'function' && this.act(doexpr);
 	}
-	pipe.getList = getList;
-
-	function Proce(doexpr, config, cleared) { }
+	Proce.prototype = {
+		id: NaN,
+		queuetodo: null,
+		queueordo: null,
+		cleared: false,
+		acted: false,
+		nmArg: false,
+		lastRtn: [],
+		lastErr: null,
+		lastDef: null,
+		config: null,
+		pointer: -1,
+		before: null,
+		uncaught: true,
+		doRtn: function (expr, param) {
+			if (this.nmArg) return expr(param);
+			else {
+				this.nmArg = true;
+				return apply(expr, null, param);
+			}
+		},
+		act: function (doexpr, args) {
+			var t = this, params = [
+				function () { if (noClear) noClear = false, t.clear(arguments); },
+				function () { if (noClear) noClear = false, t.clear(t.exeordo(arguments)); }
+			], noClear = true;
+			if (args) for (var i = 0; i < args.length; i++) params.push(args[i]);
+			try {
+				var r = apply(doexpr, this, params);
+				return this.config.trap !== 'none' && isThenable(r) && (typeof r.getBefore === 'function'
+					? r.getBefore(new ProceArray())[
+						this.config.trap === 'all' ? 'trap' : 'supp'
+					](params[1])
+					: r.then(null, params[1])
+				), this.acted = true, r;
+			} catch (errObj) { this.acted = true; return params[1](errObj); }
+		},
+		clear: function (param) {
+			var i = this.pointer, q = this.queuetodo, f = true, t = this;
+			while (++i < q.length) if (typeof q[i] === 'function') {
+				q[i].hidden || (f = false);
+				try { param = this.doRtn(q[i], param); }
+				catch (errObj) { this.pointer = i, param = this.exeordo(errObj), i = this.pointer; }
+			}
+			f && (this.lastDef = setTimeout(function () {
+				t.then(t.config.todo, t.config.ordo);
+			})), this.lastRtn = param, this.cleared = true;
+		},
+		exeordo: function (param) {
+			var i = this.pointer, q = this.queueordo;
+			while (++i < q.length) if (typeof q[i] === 'function') {
+				this.pointer = i;
+				try { return this.doRtn(q[i], param); }
+				catch (errObj) { return this.exeordo(errObj); }
+			}
+			return this.pointer = i, this.toss(this.nmArg ? param : param[0]), param;
+		},
+		toss: function (errObj) {
+			var t = this;
+			this.lastErr = setTimeout(function () {
+				if (t.lastDef !== null) return t.toss(errObj);
+				else if (isBrowser && !window.console) throw errObj;
+				else return console.error('scpo-proce Uncaught', errObj);
+			});
+		}
+	}
+	pipe.Proce = Proce;
 
 	function pipe() { }
 }());
