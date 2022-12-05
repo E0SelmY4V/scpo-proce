6e62a2668b705b9e460c7cbf35856378161b9cd2
E0SelmY4V-PC <2507917746@qq.com>
2022-12-05 13:27:17 +0800

    add: 完整Proce

--- main.d.ts
+++ main.d.ts
@@ -14,6 +14,7 @@ export namespace scpoProce {
 	type ProceN = Proce<any[], any[]>
 	type ProceArgs<T> = T extends Proce<infer A, any[]> ? A : [T]
 	type ProceFilled<T extends number, A extends ProceN[] = []> = T extends 0 ? A : ProceFilled<SntXcrNum<9, T, number>, [ProceN, ...A]>
+	type ProceTaked<T, D extends number = -1> = D extends 0 ? T : T extends Proce<[infer F extends ProceN, ...any[]], any[]> ? ProceTaked<F, SntXcrNum<9, D, number>> : T
 	function getId(): number
 	class Proce<P extends any[] = any[], E extends any[] = [any]> {
 		constructor(doexpr?: CbCur<P, E>, config?: Config, cleared?: boolean)
@@ -36,6 +37,26 @@ export namespace scpoProce {
 		clear(param: P | P[0]): void
 		exeordo(param: E | E[0]): any
 		toss: (errObj: E[0]) => void
+		getBefore(n?: ProceArray): ProceArray
+		setBefore(n: ProceArray | ProceN[]): void
+		tpus<R>(todo?: CbNor<P, R>, ordo?: CbNor<E, R>): void
+		then<R>(todo?: CbNor<P, R>, ordo?: CbNor<E, R>): Proce<[R]>
+		trap<R>(ordo?: CbNor<E, R>): Proce<[R]>
+		next<P1 extends any[], E1 extends any[], R>(doexpr?: CbNxt<P1, P, E1, R>, ordo?: CbNor<E1, R>, config?: Config): Proce<P1, E1>
+		take<D extends number = -1>(depth?: D): t.tp<P, E, D>
+		take<R, D extends number = -1, T1 = t.tp<P, E, D>>(todo: t.tf<T1, R, 0>, ordo?: t.tf<T1, R, 1>, depth?: D): Proce<[R]>
+		conf<E1>(config?: Config, ordo?: CbNor<E, E1>): t.cp<P, E1>
+		configAll(n?: Config): Proce<P, E>
+	}
+	function then<R>(todo?: CbNor<[], R>, ordo?: CbNor<[], R>): Proce<[R]>
+	function trap<R>(ordo?: CbNor<[], R>): Proce<[R]>
+	function next<P1 extends any[], E1 extends any[], R>(doexpr?: CbNxt<P1, [], E1, R>, ordo?: CbNor<E1, R>, config?: Config): Proce<P1, E1>
+	function conf<E1>(config?: Config, ordo?: CbNor<[], E1>): t.cp<[], E1>
+	function configAll(): Proce<[], []>
+	namespace t {
+		type tp<P extends any[], E extends any[], D extends number> = ProceTaked<Proce<P, E>, D>
+		type tf<T1, R, W> = CbNor<T1 extends Proce<infer P, infer E> ? W extends 0 ? P : E : W extends 0 ? [] : [any], R>
+		type cp<P extends any[], E1> = Proce<P extends [] ? [] : [P[0]], [E1]>
 	}
 	class ProceArray<P extends any[] = any[], E extends any[] = [any]> extends Array<Proce<P, E>> {
 		constructor(...proce: Proce<P, E>[])
