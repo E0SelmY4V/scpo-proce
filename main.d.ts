f92b51798870ddc2065b1c5ef80a7ce57398edc6
E0SelmY4V-PC <2507917746@qq.com>
2023-01-21 00:00:09 +0800

    Ver 1.10120.0

--- main.d.ts
+++ main.d.ts
@@ -113,7 +113,7 @@ declare namespace scpoProce {
 }
 /**
  * 幻想私社异步过程类
- * @version 1.10116.0
+ * @version 1.10120.0
  * @license GPL-3.0-or-later
  * @link https://github.com/E0SelmY4V/scpo-proce
  */
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
  * 幻想私社异步过程类
- * @version 1.10116.0
+ * @version 1.10120.0
  * @license GPL-3.0-or-later
  * @link https://github.com/E0SelmY4V/scpo-proce
  */
