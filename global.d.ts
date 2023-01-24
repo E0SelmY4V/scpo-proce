daa36d022c96a79186ecd4a521a19fd41c99038f
E0SelmY4V-PC <2507917746@qq.com>
2023-01-24 15:49:04 +0800

    Ver 2.10124.0

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,6 @@
 /**
  * 幻想私社异步过程类
- * @version 1.10120.1
+ * @version 2.10124.0
  * @license GPL-3.0-or-later
  * @link https://github.com/E0SelmY4V/scpo-proce
  */
--- main.d.ts
+++ main.d.ts
@@ -204,7 +204,7 @@ declare namespace scpoProce {
 }
 /**
  * 幻想私社异步过程类
- * @version 1.10120.1
+ * @version 2.10124.0
  * @license GPL-3.0-or-later
  * @link https://github.com/E0SelmY4V/scpo-proce
  */
